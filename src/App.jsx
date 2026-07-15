9d0b2d86af4ea18d7e4c60fdfe9797c9abd1db12
Karthikeyan-K07dechub <karthik@dechub.in>
2026-07-15 13:05:08 +0530

    New mobile container bc image addition - placement restore

--- src/App.jsx
+++ src/App.jsx
@@ -149,7 +149,7 @@ function MobileHomePage() {
 
   return (
     <ScreenPage background={mobilePageBackground} className="mobile-home-page">
-      <div className="mobile-shell mobile-shell-home">
+      <div className="mobile-shell">
         <h1 className="mobile-home-title">
           Write my wishes to Tanishq
           <br />
